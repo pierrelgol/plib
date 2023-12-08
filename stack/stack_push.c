7f745605f13751fbe101d84b5b23b253f6502467
PierreLgol <plgol.perso@gmail.com>
2023-12-08 10:00:06 +0100

    implemented stack_push function

--- stack/stack_push.c
+++ stack/stack_push.c
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../plib.h"
 
+void *stack_push(t_stack *self, void *data)
+{
+	unsigned int i;
+	t_list *new;
+
+	if (!self)
+		return (0);
+	if (self->count == self->size)
+	{
+		i = 0;
+		while (i < (self->size << 1))
+			list_insert_front(&self->free_node, data);
+		self->size <<= 1;
+		stack_push(self, data);
+	}
+	new = list_pop_front(&self->free_node);
+	new->data = data;
+	list_push_front(&self->top, new);
+	self->count += 1;
+	return (data);
+}
