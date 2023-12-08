bcfeb5d274e4d8d48f6a4f0a1835198aaab91fa3
PierreLgol <plgol.perso@gmail.com>
2023-12-08 10:05:53 +0100

    added stack_pop function

--- stack/stack_pop.c
+++ stack/stack_pop.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../plib.h"
 
+void *stack_pop(t_stack *stack)
+{
+	void	*data;
+	t_list	*node;
+
+	if (!stack || stack_is_empty(stack))
+		return (0);
+	node = list_pop_front(&stack->top);
+	data = node->data;
+	node->data = 0;
+	list_push_front(&stack->free_node, node);
+	stack->count -= 1;
+	return (data);
+}
