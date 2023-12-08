52213e38853799db2f8bcb5a6064532d3f88c002
PierreLgol <plgol.perso@gmail.com>
2023-12-08 10:07:00 +0100

    few changes namin/added function call

--- stack/stack_push.c
+++ stack/stack_push.c
@@ -12,24 +12,24 @@
 
 #include "../plib.h"
 
-void *stack_push(t_stack *self, void *data)
+void *stack_push(t_stack *stack, void *data)
 {
 	unsigned int i;
 	t_list *new;
 
-	if (!self)
+	if (!stack)
 		return (0);
-	if (self->count == self->size)
+	if (stack_is_full(stack))
 	{
 		i = 0;
-		while (i < (self->size << 1))
-			list_insert_front(&self->free_node, data);
-		self->size <<= 1;
-		stack_push(self, data);
+		while (i < (stack->size << 1))
+			list_insert_front(&stack->free_node, data);
+		stack->size <<= 1;
+		stack_push(stack, data);
 	}
-	new = list_pop_front(&self->free_node);
+	new = list_pop_front(&stack->free_node);
 	new->data = data;
-	list_push_front(&self->top, new);
-	self->count += 1;
+	list_push_front(&stack->top, new);
+	stack->count += 1;
 	return (data);
 }
