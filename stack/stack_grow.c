cbe315b2f3bc84f312f92ee034b03549c1c33d3d
PierreLgol <plgol.perso@gmail.com>
2023-12-08 11:28:31 +0100

    modification to the stack push functions, corrected a bug where the function would duplicate the data pointer when adding more capacity, due to poor logic from my end. extracted the growth process of the stack into it's own function for the sake of simplicity

--- stack/stack_grow.c
+++ stack/stack_grow.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_push.c                                       :+:      :+:    :+:   */
+/*   stack_grow.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plgol.perso <pollivie@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/05 21:05:02 by plgol.perso       #+#    #+#             */
-/*   Updated: 2023/12/05 21:05:03 by plgol.perso      ###   ########.fr       */
+/*   Created: 2023/12/08 11:23:36 by plgol.perso       #+#    #+#             */
+/*   Updated: 2023/12/08 11:23:37 by plgol.perso      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../plib.h"
 
-void *stack_push(t_stack *stack, void *data)
+t_stack *stack_grow(t_stack *stack)
 {
-	t_list *new;
+	unsigned int i;
 
 	if (!stack)
 		return (0);
-	if (stack_is_full(stack))
-		stack = stack_grow(stack);
-	new = list_pop_front(&stack->free_node);
-	new->data = data;
-	list_push_front(&stack->top, new);
-	stack->count += 1;
-	return (data);
+	i = 0;
+	while (i < (stack->size << 1))
+	{
+		list_insert_front(&stack->free_node, 0);
+		++i;
+	}
+	stack->size <<= 1;
+	return (stack);
 }
